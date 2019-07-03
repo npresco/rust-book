08b0563085d3c39ff95d80c13c4286294b952c2d
Noah Prescott <noah.prescott@gmail.com>
2019-07-02 22:51:58 -0400

    Start ownership chapter

---
+++ ownership/target/debug/deps/ownership-5101c005c5d828bf.d
@@ -0,0 +1,5 @@
+/Users/npresco/Projects/tutorials/rust/rust-book/ownership/target/debug/deps/ownership-5101c005c5d828bf: src/main.rs
+
+/Users/npresco/Projects/tutorials/rust/rust-book/ownership/target/debug/deps/ownership-5101c005c5d828bf.d: src/main.rs
+
+src/main.rs:
